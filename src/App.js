a805b9c30205b0ca40876611d8b35c7443dfc506
DickensJuma <dickensjuma13@gmail.com>
2019-08-02 11:38:45 +0300

    added profile(exercise on props)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import LoginControl from './login';
+import Profile from './Profile';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,7 +23,9 @@ class App extends React.Component {
       <button onClick={this.handleClick}>
         {this.state.isToggleOn ? 'ON' : 'OFF'}
       </button>
-      <LoginControl/></span>
+      <LoginControl/>
+      <Profile logo_url="https://scontent.fnbo1-1.fna.fbcdn.net/v/t1.0-9/67466835_1117185668482304_4136798481010393088_n.jpg?_nc_cat=110&_nc_oc=AQkCZ5s2kJ-BKrEAA5y3S9eS74IwXTCXGaNJL0CCocSdXro_zlePh50JgwIzVXtYIv0&_nc_ht=scontent.fnbo1-1.fna&oh=92d1d348a436179da0d148e536bfb01b&oe=5DE7697F"
+     title="Mobile App, Web App and API Development and More"/></span>
     );
   }
 }
