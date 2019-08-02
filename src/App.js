379e8d651dd3a569723f71749256fdcfa40b1131
DickensJuma <dickensjuma13@gmail.com>
2019-08-02 12:11:34 +0300

    updated the count file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import LoginControl from './login';
 import Profile from './Profile';
+import Count from "./Count"
 
 class App extends React.Component {
   constructor(props) {
@@ -24,6 +25,7 @@ class App extends React.Component {
         {this.state.isToggleOn ? 'ON' : 'OFF'}
       </button>
       <LoginControl/>
+      <Count/>
       <Profile logo_url="https://scontent.fnbo1-1.fna.fbcdn.net/v/t1.0-9/67466835_1117185668482304_4136798481010393088_n.jpg?_nc_cat=110&_nc_oc=AQkCZ5s2kJ-BKrEAA5y3S9eS74IwXTCXGaNJL0CCocSdXro_zlePh50JgwIzVXtYIv0&_nc_ht=scontent.fnbo1-1.fna&oh=92d1d348a436179da0d148e536bfb01b&oe=5DE7697F"
      title="Mobile App, Web App and API Development and More"/></span>
     );
---
+++ src/Count.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+class Count extends React.Component {
+ 
+  constructor(props){
+      super(props);
+      this.state={ count: 1}
+  }
+
+ onclick(type){
+     this.setState(prevState => {
+        return {count: type === 'add' ? prevState.count + 1: prevState.count - 1}
+     });
+ }
+
+  render() {
+   return (
+     <div>
+       Count: {this.state.count}
+       <br/>
+       <div style={{marginTop: '100px'}}/>
+       <input type='button' onClick={this.onclick.bind(this, 'add')} value='Increase'/>
+       <input type='button' onClick={this.onclick.bind(this, 'sub')} value='Decrease'/>
+      </div>
+    )
+  }
+}
+
+
+export default Count;
